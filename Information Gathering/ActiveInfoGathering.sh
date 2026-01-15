823480655172184481952a6360e4280d95601ef7
Yaman.RedTeam <yamanblackhat7@gmail.com>
2026-01-16 00:47:03 +0530

    update

--- Information Gathering/ActiveInfoGathering.sh
+++ Information Gathering/ActiveInfoGathering.sh
@@ -6,4 +6,13 @@
 #dns utitlity lookup
 
 =fierce -dns 
-#
+# A DNS reconnaissance tool for locating non-contiguous IP space.
+
+=ip a s
+#to see own system network ip address
+
+=nmap
+ #Network exploration tool and security / port scanner
+
+=netdiscover
+#Active/passive ARP reconnaissance tool
